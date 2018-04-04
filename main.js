9b9566de91be6342949271bba46809ea3eec0727
Rebecca Weir <rebeccaweir12@gmail.com>
2018-04-04 11:47:22 -0400

    Style and implement use of size buttons

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
-//have a button to make the image larger or smaller or normal with remove and add class
-
 $l(playDemo);
 
 function playDemo() {
-  const button = $l('button');
-  button.on("click", e => {
+  const generate = $l('.generate');
+  const image = $l('.image-to-be-gen');
+
+  const small = $l('.small');
+  const normal = $l('.normal');
+  const large = $l('.large');
+
+  const CLASSLIST = ['small', 'normal', 'large'];
+
+  generate.on("click", e => {
     $l.ajax({
       url:
       `https://api.giphy.com/v1/gifs/random?tag=parks+and+rec&api_key=L2yCnV4B058nsp7fHBXAtCUhXKIa6A8v&rating=G`,
       success(img) {
-        const image = $l('section').find('img');
-        image.attr('src', `${JSON.parse(img).data.images.downsized_large.url}`);
+        image.attr('src', `${JSON.parse(img).data.images.original.url}`);
       }
+    }).then(() => {
+      CLASSLIST.forEach(className => {
+        image.removeClass(className);
+      });
+
+      image.addClass("normal");
+      small.addClass("show");
+      normal.addClass("show");
+      large.addClass("show");
     });
+  });
+
+  small.on("click", e => {
+    CLASSLIST.forEach(className => {
+      image.removeClass(className);
+    });
+    image.addClass("small");
+  });
+
+
+  normal.on("click", e => {
+    CLASSLIST.forEach(className => {
+      image.removeClass(className);
+    });
+    image.addClass("normal");
+  });
+
+  large.on("click", e => {
+    CLASSLIST.forEach(className => {
+      image.removeClass(className);
+    });
+    image.addClass("large");
   });
 }
